435e5a01c00cbaa3781567ffc8e25ad6ec7484f9
ytanaka1985 <y_tanaka_ym@yahoo.co.jp>
2016-09-07 18:48:50 +0900

    First

---
+++ app/src/main/java/jp/techacademy/tanaka/yousuke/qa_app/LoginActivity.java
@@ -0,0 +1,13 @@
+package jp.techacademy.tanaka.yousuke.qa_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
