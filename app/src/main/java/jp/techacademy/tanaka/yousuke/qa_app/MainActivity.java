8dfacc75150803a5ce017840544744ca20fcb7e4
ytanaka1985 <y_tanaka_ym@yahoo.co.jp>
2016-09-12 21:30:58 +0900

    5.5 MainActivity.javaの修正

--- app/src/main/java/jp/techacademy/tanaka/yousuke/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/tanaka/yousuke/qa_app/MainActivity.java
@@ -1,29 +1,95 @@
 package jp.techacademy.tanaka.yousuke.qa_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ *修正する内容は以下の通りです。
+
+ ・ドロワーを表示するためのボタンを表示する
+ ・ドロワー内のメニューが選択された時にどのメニューが選択されたかを保持する
+ ・ドロワー内のメニューが選択された時にタイトルを設定する
+
+ メンバ変数としてmToolbarとmGenreを定義します。
+
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private int mGenre = 0;
+
     @Override
+    /**
+     * onCreateメソッドでfindViewByIdを使って取得している箇所をmToolbarを使うように修正します。
+     そしてドロワーの設定を追加します。
+     修正したら、実行してドロワーが表示され、メニューを選択するとタイトルが変更されることを確認しましょう。
+     *
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // ログイン済みのユーザーを収録する
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // ログインしていなければログイン画面に遷移させる
+                if (user == null) {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        // ナビゲーションドロワーの設定
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_hobby) {
+                    mToolbar.setTitle("趣味");
+                    mGenre = 1;
+                } else if (id == R.id.nav_life) {
+                    mToolbar.setTitle("生活");
+                    mGenre = 2;
+                } else if (id == R.id.nav_health) {
+                    mToolbar.setTitle("健康");
+                    mGenre = 3;
+                } else if (id == R.id.nav_compter) {
+                    mToolbar.setTitle("コンピューター");
+                    mGenre = 4;
+                }
+
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
     }
